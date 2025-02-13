c37fd6a46e50556dd2ca42977a787af54a8a66ac
rizkiraden <rizkiraden616@gmail.com>
2025-02-13 15:51:44 +0700

    Menambahkan halaman opsi autentikasi

---
+++ app/sipreti/lib/pages/option_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class OptionPage extends StatelessWidget {
+  const OptionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double widthScreen = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Column(
+              children: [
+                Expanded(child: Container()),
+                Image.asset(
+                  'assets/images/pemkot_malang_bg.png',
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/pemkot_malang_logo.png',
+                  width: 160,
+                  height: 160,
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "PEMKOT MALANG",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 80),
+                const Text(
+                  "PRESENSI ONLINE\nPEMERINTAH\nKOTA MALANG",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                const SizedBox(height: 80),
+                SizedBox(
+                  width: widthScreen * 0.8,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: const BorderSide(color: Colors.blue),
+                      ),
+                      shadowColor: Colors.black,
+                      elevation: 5,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "DAFTAR",
+                      style: TextStyle(fontSize: 18, color: Colors.blue),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: widthScreen * 0.8,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: const BorderSide(color: Colors.white),
+                      ),
+                      shadowColor: Colors.black,
+                      elevation: 5,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "MASUK",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
