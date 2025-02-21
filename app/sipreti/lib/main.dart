173e9d39826f76e0e8adfe0260fea9d1f07b2fd5
rizkiraden <rizkiraden616@gmail.com>
2025-02-21 13:44:10 +0700

    Menambahkan halaman Beranda

--- app/sipreti/lib/main.dart
+++ app/sipreti/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sipreti/pages/dashboard_page.dart';
 import 'package:sipreti/pages/form_page.dart';
 import 'package:sipreti/pages/nip_page.dart';
 import 'package:sipreti/pages/option_page.dart';
 
 Future<void> main() async {
-  const initialRoute = '/form';
+  const initialRoute = '/';
 
   runApp(MaterialApp(
     initialRoute: initialRoute,
     routes: {
+      '/': (context) => const DashboardPage(),
       '/option': (context) => const OptionPage(),
       '/nip': (context) => const NIPPage(),
       '/form': (context) => const FormPage(),
---
+++ app/sipreti/lib/pages/dashboard_page.dart
@@ -0,0 +1,340 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  DashboardPageState createState() => DashboardPageState();
+}
+
+class DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF0ECEC),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Presensi Online",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onPressed: () {},
+        ),
+        actions: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/images/pemkot_malang_logo.png',
+                height: 32,
+              ),
+              const SizedBox(width: 8),
+              const CircleAvatar(
+                backgroundImage:
+                    AssetImage('assets/images/default_profile.png'),
+                radius: 16,
+              ),
+              const SizedBox(width: 10),
+            ],
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 260,
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            child: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage:
+                      AssetImage('assets/images/default_profile.png'),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "AMAR BUDIMAN",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "NIP: 109088216612001290",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Kesekretariatan - Kepegawaian -\nAnalis Sumber Daya Manusia Aparatur",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                SizedBox(height: 8),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 220),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.black12, blurRadius: 5),
+                      ],
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Check In terakhir",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFFABABAB)),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                "07:30",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Check Out terakhir",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFFABABAB)),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                "-",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.black12, blurRadius: 5),
+                      ],
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          flex: 2,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const SizedBox(width: 12),
+                              _buildPieChart(75, Colors.blue, "75.0%"),
+                              const SizedBox(width: 12),
+                              _buildPieChart(25, Colors.red, "25.0%"),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Flexible(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    "Index Presensi Januari",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFFABABAB),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Column(
+                                  children: [
+                                    _buildLegend(Colors.blue, "Kehadiran"),
+                                    const SizedBox(width: 10),
+                                    _buildLegend(Colors.red, "Sakit"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              child: _buildButton(
+                                  "Check In", Icons.login, Colors.blue)),
+                          const SizedBox(width: 10),
+                          Expanded(
+                              child: _buildButton(
+                                  "Check Out", Icons.logout, Colors.blue)),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                              child: _buildButton(
+                                  "Laporan", Icons.insert_chart, Colors.blue)),
+                          const SizedBox(width: 10),
+                          Expanded(
+                              child: _buildButton(
+                                  "Data Pegawai", Icons.people, Colors.blue)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "Riwayat"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Pesan"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Akun"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPieChart(int percentage, Color color, String label) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                width: 50,
+                height: 50,
+                child: CircularProgressIndicator(
+                  value: percentage / 100,
+                  backgroundColor: Colors.grey[300],
+                  color: color,
+                  strokeWidth: 5,
+                ),
+              ),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    label,
+                    style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.bold,
+                      color: color,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLegend(Color color, String label) {
+    return Row(
+      children: [
+        Container(
+          width: 12,
+          height: 12,
+          decoration: BoxDecoration(
+              color: color, borderRadius: BorderRadius.circular(2)),
+        ),
+        const SizedBox(width: 5),
+        Text(label,
+            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+}
+
+Widget _buildButton(String text, IconData icon, Color iconColor) {
+  return Container(
+    padding: const EdgeInsets.all(12),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: const [
+        BoxShadow(color: Colors.black12, blurRadius: 5),
+      ],
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: iconColor),
+        const SizedBox(width: 8),
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+  );
+}
