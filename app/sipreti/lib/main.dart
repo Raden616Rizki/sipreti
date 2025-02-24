2950cb55359b5519955c5dc87d58ba8dea6a3ee7
rizkiraden <rizkiraden616@gmail.com>
2025-02-24 20:40:57 +0700

    Setup Dipendensi dan Versi Android

--- app/sipreti/lib/main.dart
+++ app/sipreti/lib/main.dart
@@ -6,12 +6,13 @@ import 'package:sipreti/pages/nip_page.dart';
 import 'package:sipreti/pages/option_page.dart';
 
 Future<void> main() async {
-  const initialRoute = '/attendance';
+  const initialRoute = '/location';
 
   runApp(MaterialApp(
     initialRoute: initialRoute,
     routes: {
       '/': (context) => const DashboardPage(),
+      '/location': (context) => const DashboardPage(),
       '/attendance': (context) => const AttendancePage(),
       '/option': (context) => const OptionPage(),
       '/nip': (context) => const NIPPage(),
