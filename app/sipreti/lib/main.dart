1ab5cc6c5d733a4e670dd4549ed9bbf4c003bc59
rizkiraden <rizkiraden616@gmail.com>
2025-02-25 11:38:30 +0700

    Menambahkan halaman Biometrik

--- app/sipreti/lib/main.dart
+++ app/sipreti/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:sipreti/pages/attendance_page.dart';
+import 'package:sipreti/pages/biometric_page.dart';
 import 'package:sipreti/pages/dashboard_page.dart';
+import 'package:sipreti/pages/location_page.dart';
+import 'package:sipreti/pages/attendance_page.dart';
 import 'package:sipreti/pages/form_page.dart';
 import 'package:sipreti/pages/nip_page.dart';
 import 'package:sipreti/pages/option_page.dart';
 
 Future<void> main() async {
-  const initialRoute = '/location';
+  const initialRoute = '/biometric';
 
   runApp(MaterialApp(
     initialRoute: initialRoute,
     routes: {
       '/': (context) => const DashboardPage(),
-      '/location': (context) => const DashboardPage(),
+      '/location': (context) => const LocationPage(),
+      '/biometric': (context) => const BiometricPage(),
       '/attendance': (context) => const AttendancePage(),
       '/option': (context) => const OptionPage(),
       '/nip': (context) => const NIPPage(),
---
+++ app/sipreti/lib/pages/biometric_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class BiometricPage extends StatefulWidget {
+  const BiometricPage({super.key});
+
+  @override
+  State<BiometricPage> createState() => _BiometricPageState();
+}
+
+class _BiometricPageState extends State<BiometricPage> {
+  CameraController? _cameraController;
+  List<CameraDescription>? cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    cameras = await availableCameras();
+    if (cameras != null && cameras!.isNotEmpty) {
+      _cameraController = CameraController(
+        cameras![1],
+        ResolutionPreset.high,
+      );
+
+      await _cameraController!.initialize();
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "Verifikasi Wajah",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        automaticallyImplyLeading: false,
+        actions: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/images/pemkot_malang_logo.png',
+                height: 32,
+              ),
+              const SizedBox(width: 8),
+              const CircleAvatar(
+                backgroundImage:
+                    AssetImage('assets/images/default_profile.png'),
+                radius: 16,
+              ),
+              const SizedBox(width: 10),
+            ],
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          if (_cameraController != null &&
+              _cameraController!.value.isInitialized)
+            SizedBox(
+              width: screenWidth,
+              height: screenHeight,
+              child: AspectRatio(
+                aspectRatio: _cameraController!.value.aspectRatio,
+                child: CameraPreview(_cameraController!),
+              ),
+            )
+          else
+            const Center(child: CircularProgressIndicator()),
+          Positioned(
+            bottom: 50,
+            left: screenWidth / 2 - 30,
+            child: GestureDetector(
+              // onTap: _takePicture,
+              child: Image.asset(
+                'assets/images/camera_button.png',
+                width: 80,
+                height: 80,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
