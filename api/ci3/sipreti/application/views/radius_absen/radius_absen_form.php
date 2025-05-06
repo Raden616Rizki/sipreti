56e52e27c674d5641eac747ff6c1c2deae4ae9b4
rizkiraden <rizkiraden616@gmail.com>
2025-05-06 23:54:28 +0700

    Memperbarui antarmuka radius absen

--- api/ci3/sipreti/application/views/radius_absen/radius_absen_form.php
+++ api/ci3/sipreti/application/views/radius_absen/radius_absen_form.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
     <head>
-        <title>harviacode.com - codeigniter crud generator</title>
+        <title>Kelola Radius Absen</title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <style>
             body{
@@ -10,7 +10,7 @@
         </style>
     </head>
     <body>
-        <h2 style="margin-top:0px">Radius_absen <?php echo $button ?></h2>
+        <h2 style="margin-top:0px">Radius Absen <?php echo $button ?></h2>
         <form action="<?php echo $action; ?>" method="post">
 	    <div class="form-group">
             <label for="float">Ukuran <?php echo form_error('ukuran') ?></label>
--- api/ci3/sipreti/application/views/radius_absen/radius_absen_read.php
+++ api/ci3/sipreti/application/views/radius_absen/radius_absen_read.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
     <head>
-        <title>harviacode.com - codeigniter crud generator</title>
+        <title>Detail Radius Absen</title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <style>
             body{
@@ -10,13 +10,10 @@
         </style>
     </head>
     <body>
-        <h2 style="margin-top:0px">Radius_absen Read</h2>
+        <h2 style="margin-top:0px">Detail Radius Absen</h2>
         <table class="table">
 	    <tr><td>Ukuran</td><td><?php echo $ukuran; ?></td></tr>
 	    <tr><td>Satuan</td><td><?php echo $satuan; ?></td></tr>
-	    <tr><td>Created At</td><td><?php echo $created_at; ?></td></tr>
-	    <tr><td>Updated At</td><td><?php echo $updated_at; ?></td></tr>
-	    <tr><td>Deleted At</td><td><?php echo $deleted_at; ?></td></tr>
 	    <tr><td></td><td><a href="<?php echo site_url('radius_absen') ?>" class="btn btn-default">Cancel</a></td></tr>
 	</table>
         </body>
