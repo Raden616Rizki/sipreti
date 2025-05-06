21119f0d5ab96b6d60f76ae2f2e5028cb859b3b5
rizkiraden <rizkiraden616@gmail.com>
2025-05-06 23:54:13 +0700

    Memperbarui antarmuka jabatan

--- api/ci3/sipreti/application/views/jabatan/jabatan_read.php
+++ api/ci3/sipreti/application/views/jabatan/jabatan_read.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
     <head>
-        <title>harviacode.com - codeigniter crud generator</title>
+        <title>Detail Jabatan</title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <style>
             body{
@@ -10,12 +10,12 @@
         </style>
     </head>
     <body>
-        <h2 style="margin-top:0px">Jabatan Read</h2>
+        <h2 style="margin-top:0px">Detail Jabatan</h2>
         <table class="table">
 	    <tr><td>Nama Jabatan</td><td><?php echo $nama_jabatan; ?></td></tr>
-	    <tr><td>Created At</td><td><?php echo $created_at; ?></td></tr>
+	    <!-- <tr><td>Created At</td><td><?php echo $created_at; ?></td></tr>
 	    <tr><td>Updated At</td><td><?php echo $updated_at; ?></td></tr>
-	    <tr><td>Deleted At</td><td><?php echo $deleted_at; ?></td></tr>
+	    <tr><td>Deleted At</td><td><?php echo $deleted_at; ?></td></tr> -->
 	    <tr><td></td><td><a href="<?php echo site_url('jabatan') ?>" class="btn btn-default">Cancel</a></td></tr>
 	</table>
         </body>
