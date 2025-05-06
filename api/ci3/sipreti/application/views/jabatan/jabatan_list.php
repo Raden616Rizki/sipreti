9fa4a1e19b99c603c0c12c95ad85776ca79989fc
rizkiraden <rizkiraden616@gmail.com>
2025-05-07 00:34:01 +0700

    Pengujian Github Workflows

--- api/ci3/sipreti/application/views/jabatan/jabatan_list.php
+++ api/ci3/sipreti/application/views/jabatan/jabatan_list.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
     <head>
-        <title>Daftar Jabatan Pegawai</title>
+        <title>Daftar Jabatan</title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <style>
             body{
