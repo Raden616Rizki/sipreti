e863f2db4eef3a9f9dbabca6997668567bbeac43
rizkiraden <rizkiraden616@gmail.com>
2025-05-26 16:06:52 +0700

    Pengujian Model Baru

--- api/django/sipreti/attendance/face_verification/extraction.py
+++ api/django/sipreti/attendance/face_verification/extraction.py
@@ -24,7 +24,8 @@ def get_drive_service():
 
 # Fungsi untuk memuat model MobileFaceNet (TFLite)
 def load_tflite_model():
-    model_path = os.path.join(settings.BASE_DIR, "assets/model/mobilefacenet.tflite")
+    # model_path = os.path.join(settings.BASE_DIR, "assets/model/mobilefacenet.tflite")
+    model_path = os.path.join(settings.BASE_DIR, "assets/model/small_mobilefacenet.tflite")
     interpreter = tf.lite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
     return interpreter
